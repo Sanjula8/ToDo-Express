fd15b0da4a07ae3e901ba8517073df81aa8f82ad
Sanjula <sanjula8@gmail.com>
2020-07-01 12:07:32 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+app = express();
+port = process.env.PORT || 3000;
+
+var todoRoutes = require("./routes/todos");
+
+app.get("/", function (req, res) {
+	res.send("HELLO FROM ROOT ROUTE");
+});
+
+app.listen(port, function () {
+	console.log("APP IS RUNNING ON PORT " + port);
+});
