a0519af9d6792c3b36f890de5429356044a5b042
Sanjula <sanjula8@gmail.com>
2020-07-01 16:38:05 -0500

    connected form to API

--- public/app.js
+++ public/app.js
@@ -1,16 +1,38 @@
 $(document).ready(function () {
 	$.getJSON("/api/todos").then(addTodos);
+	$("#todoInput").keypress(function (event) {
+		if (event.which == 13) {
+			createTodo();
+		}
+	});
 });
 
 function addTodos(todos) {
 	//add todos to page
 	todos.forEach(function (todo) {
-		var newTodo = $(
-			'<li class="task">' + todo.name + todo.completed + "</li>"
-		);
-		if (todo.completed) {
-			newTodo.addClass("done");
-		}
-		$(".list").append(newTodo);
+		addTodo(todo);
 	});
+}
+
+function addTodo(todo) {
+	var newTodo = $('<li class="task">' + todo.name + "</li>");
+	if (todo.completed) {
+		newTodo.addClass("done");
+	}
+	$(".list").append(newTodo);
+}
+
+function createTodo() {
+	//send request to create new todo
+	var userInput = $("#todoInput").val();
+	// console.log(userInput);
+	$.post("/api/todos", { name: userInput })
+		.then(function (newTodo) {
+			// console.log(newTodo);
+			$("#todoInput").val("");
+			addTodo(newTodo);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
 }
