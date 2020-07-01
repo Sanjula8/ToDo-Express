43546051b7874f060c92b95624d492f9ffa9a0d5
Sanjula <sanjula8@gmail.com>
2020-07-01 17:04:10 -0500

    added delete button, made that work

--- public/app.js
+++ public/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-	$.getJSON("/api/todos").then(addTodos);
-	$("#todoInput").keypress(function (event) {
+	$.getJSON('/api/todos').then(addTodos);
+
+	$('#todoInput').keypress(function (event) {
 		if (event.which == 13) {
 			createTodo();
 		}
+	});
+
+	$('.list').on('click', 'span', function () {
+		removeTodo($(this).parent());
 	});
 });
 
@@ -15,22 +20,40 @@ function addTodos(todos) {
 }
 
 function addTodo(todo) {
-	var newTodo = $('<li class="task">' + todo.name + "</li>");
+	var newTodo = $(
+		'<li class="task">' + todo.name + '<span class="icon">X</span></li>'
+	);
+	newTodo.data('id', todo._id);
 	if (todo.completed) {
-		newTodo.addClass("done");
+		newTodo.addClass('done');
 	}
-	$(".list").append(newTodo);
+	$('.list').append(newTodo);
 }
 
 function createTodo() {
 	//send request to create new todo
-	var userInput = $("#todoInput").val();
+	var userInput = $('#todoInput').val();
 	// console.log(userInput);
-	$.post("/api/todos", { name: userInput })
+	$.post('/api/todos', { name: userInput })
 		.then(function (newTodo) {
 			// console.log(newTodo);
-			$("#todoInput").val("");
+			$('#todoInput').val('');
 			addTodo(newTodo);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+function removeTodo(todo) {
+	var clickedId = todo.data('id');
+	var deleteUrl = 'api/todos/' + clickedId;
+	$.ajax({
+		method: 'DELETE',
+		url: deleteUrl,
+	})
+		.then(function (data) {
+			todo.remove();
 		})
 		.catch(function (err) {
 			console.log(err);
